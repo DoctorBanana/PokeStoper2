35f58c4f9f5afa9da0171e775d8e0ff27fbcb658
Jerry Noleto De Souza <jnsouza@stefanini.com>
2020-07-09 00:11:25 -0300

    Erro na function de addShopItem

--- PokeApi.js
+++ PokeApi.js
@@ -1,5 +1,11 @@
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready);
+} else {
+  ready();
+}
+
 const poke_container = document.getElementById('Pokes');
-const pokemons_number = 7;
+const pokemons_number = 9;
 const colors = {
   fire: '#FDDFDF',
   grass: '#DEFDE0',
@@ -37,7 +43,7 @@ function createPokemonCard(pokemon) {
   const poke_types = pokemon.types.map((type) => type.type.name);
   const type = main_types.find((type) => poke_types.indexOf(type) > -1);
   const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-  const xp = pokemon.base_experience;
+  const price = pokemon.base_experience;
   const color = colors[type];
 
   pokemonEl.style.backgroundColor = color;
@@ -55,14 +61,120 @@ function createPokemonCard(pokemon) {
             <h3 class="name">${name}</h3>
             <small class="type">Type: <span>${type}</span></small> </p>
             <img width="20" height="20" src="https://vignette.wikia.nocookie.net/pokemongohelp/images/9/99/Pokecoin1.png/revision/latest/scale-to-width-down/340?cb=20160923165335" />
-            <span class="text-sm-center">P$${xp}</span> </p>
-            <button type="button" class="btn btn-danger btn-sm">Capturar no carrinho</button>
+            <span class="text-sm-center">P$${price}</span> </p>
+            <button type="button" class="btn btn-danger btn-sm shopAddButton">Capturar no carrinho</button>
         </div>
         `;
 
   pokemonEl.innerHTML = pokeInnerHTML;
 
   poke_container.appendChild(pokemonEl);
+}
+
+function ready() {
+  var removeCartItemButtons = document.getElementsByClassName('btn-warning');
+  for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i];
+    button.addEventListener('click', removeCartItem);
+  }
+
+  var quantityInputs = document.getElementsByClassName('cart-quantity-input');
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i];
+    input.addEventListener('change', quantityChanged);
+  }
+
+  var addToCartButtons = document.getElementsByClassName('shop-item-button');
+  for (var i = 0; i < addToCartButtons.length; i++) {
+    var button = addToCartButtons[i];
+    button.addEventListener('click', addToCartClicked);
+  }
+
+  document
+    .getElementsByClassName('btn-purchase')[0]
+    .addEventListener('click', purchaseClicked);
+}
+
+function purchaseClicked() {
+  alert('Cuide bem de seus Pokemon!');
+  var cartItems = document.getElementsByClassName('cart-items')[0];
+  while (cartItems.hasChildNodes()) {
+    cartItems.removeChild(cartItems.firstChild);
+  }
+  updateCartTotal();
+}
+
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.parentElement.remove();
+  updateCartTotal();
+}
+
+function quantityChanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateCartTotal();
+}
+
+function addToCartClicked(event) {
+  var button = event.target;
+  var shopItem = button.parentElement.parentElement;
+  var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText;
+  var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
+  addItemToCart(title, price);
+  updateCartTotal();
+}
+
+function addItemToCart(title, price) {
+  var cartRow = document.createElement('div');
+  cartRow.classList.add('cart-row');
+  var cartItems = document.getElementsByClassName('cart-items')[0];
+  var cartItemNames = cartItems.getElementsByClassName('cart-item-title');
+  for (var i = 0; i < cartItemNames.length; i++) {
+    if (cartItemNames[i].innerText == title) {
+      alert('Este pokemon já está capturado');
+      return;
+    }
+  }
+  var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`;
+  cartRow.innerHTML = cartRowContents;
+  cartItems.append(cartRow);
+  cartRow
+    .getElementsByClassName('btn-danger')[0]
+    .addEventListener('click', removeCartItem);
+  cartRow
+    .getElementsByClassName('cart-quantity-input')[0]
+    .addEventListener('change', quantityChanged);
+}
+
+function updateCartTotal() {
+  var cartItemContainer = document.getElementsByClassName('cart-items')[0];
+  var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+  var total = 0;
+  for (var i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i];
+    var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+    var quantityElement = cartRow.getElementsByClassName(
+      'cart-quantity-input'
+    )[0];
+    var price = parseFloat(priceElement.innerText.replace('$', ''));
+    var quantity = quantityElement.value;
+    total = total + price * quantity;
+  }
+  total = Math.round(total * 100) / 100;
+  document.getElementsByClassName('cart-total-price')[0].innerText =
+    '$' + total;
 }
 
 fetchPokemons();
